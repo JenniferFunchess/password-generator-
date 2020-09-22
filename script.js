623f4a16e8a850b9d9c0ada3212d04406d4919f4
Jennifer Funchess <jfunchess@ymail.com>
2020-09-22 00:32:49 -0400

    added condition including the number of characters you need

--- script.js
+++ script.js
@@ -11,8 +11,20 @@ function writePassword() {
 }
 
 function generatePassword(){
+  var userApproval = confirm("Please confirm the criteria for this password");
+  var characterLength = prompt("How long would you like your password to be?");
+
+  if(characterLength < 8 ){
+    alert("Needs to be more than 8 characters!")
+  }
+  else if (characterLength > 128){
+    alert ("Needs to be under 128 characters!")
+  }
   return "this will be replaced by my generated password.";
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// when click on "generate password" prompts will appear
